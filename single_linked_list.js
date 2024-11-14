c0a179f2e53b2e28cf9f0962c971c3a88eb5c77f
ed_dev <62460830+ed-dev-oc@users.noreply.github.com>
2024-11-14 21:54:05 +0800

    Create single_linked_list.js

---
+++ single_linked_list.js
@@ -0,0 +1,79 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add(data) {
+    const node = new Node(data);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+
+    this.size++;
+  }
+
+  remove(data) {
+    if (!this.head) {
+      return;
+    }
+    if (this.head.data === data) {
+      this.head = this.head.next;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      if (current.next.data === data) {
+        current.next = current.next.next;
+        return;
+      }
+      current = current.next;
+    }
+
+    this.size--;
+  }
+
+  print() {
+    let current = this.head;
+
+    let values = []
+    while (current) {
+      values.push(current.data);
+      current = current.next;
+    }
+
+    return values.join(' -> ');
+  }
+
+  search(data) {
+    let current = this.head;
+    while (current) {
+      if (current.data === data) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  size() {
+    return this.size;
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+}
